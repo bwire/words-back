76ff4cbf8ba7fcdfacc740be7b03f22887e4108e
barbedwire <e.barbedwire@gmail.com>
2023-09-09 13:21:45 +0200

    add body-parser and new-word endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 1748;
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => {
   res.send('Checking');
 });
+
+app.post('/words', (req, res) => {
+  console.log(req.body);
+
+  res.send(JSON.stringify(req.body));
+})
 
 app.listen(port, () => {
   return console.log(`Words server is listening at http://localhost:${port}`);
